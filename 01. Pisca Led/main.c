bac68fed34e0a698dd8275bb7c6b6b5373e5df2d
Jefferson Uchoa Ponte <jefponte@unilab.edu.br>
2017-06-21 23:46:12 -0300

    Primeiro projeto.

--- 01. Pisca Led/main.c
+++ 01. Pisca Led/main.c
@@ -1,17 +1,40 @@
 #include <p18f4520.h>
 
-void deley_segundos(){
+//#pragma config OSC = INTOSCIO_EC    /// EXTERN CLOCK 8MHZ
+#pragma config PBADEN=OFF
+
+
+void deley_test(){
 	long int i;
-	for(i = 0; i < 50000;i++){}
+	for(i = 0; i < 32000; i++){}
+
 }
 
+void deley_metade(){
+	long int i;
+	for(i = 0; i < 8000; i++){}
+
+}
 void main(){
+	OSCCON=0b01100010;
+	ADCON1=0x0F;
 	TRISB = 0b11111110;
-	PORTB = 0b11111110;
 	while(1){
-		PORTB = 0b11111111;
-		deley_segundos();
-		PORTB = 0b11111110;
-		deley_segundos();
+		PORTB = 0b00000000;
+		deley_metade();
+		PORTB = 0b00000001;
+		deley_metade();
+		PORTB = 0b00000000;
+		deley_metade();
+		PORTB = 0b00000001;
+		deley_metade();
+		PORTB = 0b00000000;
+		deley_metade();
+		PORTB = 0b00000001;
+		deley_test();
+		PORTB = 0b00000000;
+		deley_metade();
 	}
+
+
 }
