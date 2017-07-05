d4b2ab9f175ac1660afa5d3633d076d9cc9fe3aa
Jefferson Uchoa Ponte <jefponte@unilab.edu.br>
2017-07-05 18:50:30 -0300

    Vamos ignorar algumas coisas.

--- 02. Display LCD/main.c
+++ 02. Display LCD/main.c
@@ -4,18 +4,7 @@
 #pragma config PBADEN=OFF
 
 void main(){
-	int i, tamanho;
-	char str[20] = "Ola, Mundo!";
-	char str2[16] = "Tudo certo?";
-	OSCCON = 0b01100010;
-	ADCON1 =0x0F;
-	lcd_configura();
-
-	while(1){
-		printf(str);
-		deley_s();
-		printf(str2);
-		deley_s();
-	}
+	
+	deley_s();
 
 }
